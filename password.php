4a8ad327d3605b71eef570dadb13d72869a8fbdc
Alex Nunzi <alex.nunzi@hotmail.it>
2023-04-19 18:59:07 +0200

    Milsetone 4: Aggiunte regole di stile al file password.php

--- password.php
+++ password.php
@@ -8,12 +8,16 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Strong password generator!</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
     <main class="text-center mt-5 container">
-    <h1 class="text-primary">La password che hai generato è la seguente</h1>
-    <p><?php echo $_SESSION['newRandomPassword']; ?></p>
+        <div class="border border-black rounded-3 bg-light p-3">
+            <h1 class="text-primary">La password che hai generato è la seguente</h1>
+            <h2><?php echo $_SESSION['newRandomPassword']; ?></h2>
+            <a class="btn btn-primary" href="index.php">Torna alla home</a>
+        </div>
     </main>
 </body>
 </html>
