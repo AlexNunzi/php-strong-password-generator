4a218a660a679f2fc38e452073f171f1d902923c
Alex Nunzi <alex.nunzi@hotmail.it>
2023-04-19 18:45:12 +0200

    Milestone 4 (bonus): Aggiunti input per poter decidere con quali caratteri comporre la password con la possibilità di comporre la password anche solo con alcuni dei parametri proposti ma con l'obbligo di selezionare almeno uno tra lettere, numeri o caratteri speciali.

--- index.php
+++ index.php
@@ -21,15 +21,42 @@ include __DIR__ . '/partials/functions.php';
         <?php
             session_start();
             if(isset($_GET['pswLength']) && !empty($_GET['pswLength'])){
-                $_SESSION['newRandomPassword'] = generateNewPassword($_GET['pswLength']);
-                header('Location: password.php');
+                $uniqueCheck = (isset($_GET['uniqueCheck']) && !empty($_GET['uniqueCheck']));
+                $charactersCheck = (isset($_GET['charactersCheck']) && !empty($_GET['charactersCheck']));
+                $numbersCheck = (isset($_GET['numbersCheck']) && !empty($_GET['numbersCheck']));
+                $specialCharCheck = (isset($_GET['specialCharCheck']) && !empty($_GET['specialCharCheck']));
+                if($charactersCheck || $numbersCheck || $specialCharCheck){
+                    $_SESSION['newRandomPassword'] = generateNewPassword($_GET['pswLength'], $uniqueCheck, $charactersCheck, $numbersCheck, $specialCharCheck);
+                    header('Location: password.php');
+                }
+                else{
+                    echo '<p class="text-danger">Seleziona almeno una delle tipologie di caratteri sottostanti</p>';
+                }
             } else {
                 echo '<p>Inserisci un numero da 1 a 16 per decidere la lunghezza della password da generare</p>';
             }
         ?>
         <form class="mb-3 border border-black rounded-3 bg-light p-3" action="index.php">
-            <label for="pswLength" class="form-label">Lunghezza password:</label>
-            <input type="number" min="1" max="16" class="form-control d-inline-block w-auto" id="pswLength" name="pswLength" required>
+            <div>
+                <label for="pswLength" class="form-label mb-4">Lunghezza password:</label>
+                <input type="number" min="1" max="16" class="form-control d-inline-block w-auto" id="pswLength" name="pswLength" required>
+            </div>
+            <div>
+                <label class="form-check-label mb-4" for="uniqueCheck">Solo caratteri non ripetuti</label>
+                <input type="checkbox" class="form-check-input" id="uniqueCheck" name="uniqueCheck" value="unique">
+            </div>
+            <div>
+                <label class="form-check-label mb-4" for="charactersCheck">Lettere</label>
+                <input type="checkbox" class="form-check-input" id="charactersCheck" name="charactersCheck" value="characters">
+            </div>
+            <div>
+                <label class="form-check-label mb-4" for="numbersCheck">Numeri</label>
+                <input type="checkbox" class="form-check-input" id="numbersCheck" name="numbersCheck" value="numbers">
+            </div>
+            <div>
+                <label class="form-check-label mb-4" for="specialCharCheck">Simboli</label>
+                <input type="checkbox" class="form-check-input" id="specialCharCheck" name="specialCharCheck" value="special">
+            </div>
             <button type="submit" href="" class="btn btn-primary d-block m-auto mt-5">Genera Password</button>
         </form>
     </main>
--- partials/functions.php
+++ partials/functions.php
@@ -1,14 +1,30 @@
 <?php
 
-function generateNewPassword($length){
+function generateNewPassword($length, $unique, $characters, $numbersCheck, $special){
     $uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     $lowercaseLetters = ['a', 'b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'];
     $specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '~', '|', '<', '>', '=', '-', '_', '/', ':', ';', '?', '[', ']', '{', '}', '~'];
     $numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    $filter = array_merge($uppercaseLetters, $lowercaseLetters, $specialCharacters, $numbers);
+    $filter = []; 
+    if($characters){
+        $filter = array_merge($filter, $uppercaseLetters, $lowercaseLetters);
+    } 
+    if($numbersCheck){
+        $filter = array_merge($filter, $numbers);
+    } 
+    if($special){
+        $filter = array_merge($filter, $specialCharacters);
+    } 
     $newPassword = [];
     while(count($newPassword) < $length){
-        $newPassword[] = $filter[rand(0, (count($filter) - 1))];
+        $newChar = $filter[rand(0, (count($filter) - 1))];
+        if($unique){
+            if(!in_array($newChar, $newPassword)){
+                $newPassword[] = $newChar;
+            }
+        } else {
+            $newPassword[] = $newChar;
+        }
     }
     return implode($newPassword);
 }
